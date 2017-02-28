63c4d9671a04e956fe13cc48466bea156e64992d
divyanewt <divikvcet95@gmail.com>
2017-02-28 13:15:31 +0530

    sample

--- Sample.java
+++ Sample.java
@@ -1,12 +1,12 @@
 class Sample{
 public static void main(String args[])
 {
-System.out.println("haiii");
+System.out.println("hai");
 }
 }
 class Demo1{
 public void dex()
 {
-System.out.println("haiiiii22");
+System.out.println("hai");
 }
 }
